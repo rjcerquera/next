2a51e983731bb1f17ed5601831e8b418a532393f
JUNIOR CERQUERA <jrcerquera.n@gmail.com>
2023-10-06 17:16:13 -0500

    clean api routes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,25 +1,12 @@
 import { NextResponse } from "next/server"
 
-import {
-  getUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-} from "../../../mocks/users/data"
+import { getUsers, createUser } from "../../../mocks/users/data"
 
 export async function GET(req: Request, res: Response) {
   return NextResponse.json(getUsers())
 }
 
 export async function POST(req: Request, res: Response) {
-  const body = await req.json()
-  const newUser = body
-  createUser(newUser)
-  return NextResponse.json(newUser)
-}
-
-export async function DELETE(req: Request, res: Response) {
   const body = await req.json()
   const newUser = body
   createUser(newUser)
